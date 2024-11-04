e591731df1c2237a59c6949aa9e96b5858e7325c
noahmccammon <noahmccammon@gmail.com>
2024-11-03 21:06:29 -0800

    Hour 10

---
+++ nmcammon_Hour10/Assets/GameCritic.cs
@@ -0,0 +1,24 @@
+/*
+ ________  ________  _____ ______   _______   ________  ________  ___  _________  ___  ________     
+|\   ____\|\   __  \|\   _ \  _   \|\  ___ \ |\   ____\|\   __  \|\  \|\___   ___\\  \|\   ____\    
+\ \  \___|\ \  \|\  \ \  \\\__\ \  \ \   __/|\ \  \___|\ \  \|\  \ \  \|___ \  \_\ \  \ \  \___|    
+ \ \  \  __\ \   __  \ \  \\|__| \  \ \  \_|/_\ \  \    \ \   _  _\ \  \   \ \  \ \ \  \ \  \       
+  \ \  \|\  \ \  \ \  \ \  \    \ \  \ \  \_|\ \ \  \____\ \  \\  \\ \  \   \ \  \ \ \  \ \  \____  
+   \ \_______\ \__\ \__\ \__\    \ \__\ \_______\ \_______\ \__\\ _\\ \__\   \ \__\ \ \__\ \_______\
+    \|_______|\|__|\|__|\|__|     \|__|\|_______|\|_______|\|__|\|__|\|__|    \|__|  \|__|\|_______|
+
+Is the game too easy or hard?
+    Neither honestly, the game basically plays itself with very little input from the player so you just sit there and wait for the balls to sort themselves out.
+What would make the game easier or harder?
+    Slowing the balls down slightly might make the game easier so you actually have a chance to influence them. To make the game harder you could make the goals move along the edges of the arena.
+What would give it a "wow" factor?
+    It's a game about getting balls into goals, I don't think there's much "wow" factor to be added. But maybe some sound effects on bounces and when a ball enters a goal would help.
+What parts of the game are fun? What parts of the game are tedious?
+    As mentioned previously, you kind of just have to sit and wait for the balls to score themselves because they move too fast and too eratically for the player to have any real imput.
+
+Suggestions implemented:
+    Added a timer
+    Made the bumper larger
+    Added splash sound effect when the bumper falls into the water
+    Made the chaos balls varying sizes to make some of them more likely to collide
+*/
