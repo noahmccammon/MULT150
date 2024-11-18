19c578fc0218014daf85d5d2e9a69c382cb33673
noahmccammon <noahmccammon@gmail.com>
2024-11-18 00:59:09 -0800

    Hours 14 and 16

---
+++ nmccammon_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,9 @@
+/*
+ * Changed color of particles
+ * Particles get lighter over time
+ * Particles get smaller over time
+ * Particles have a trail
+ * Particles have collision
+ * Particles bounce
+ * It's supposed to look at least somewhat like rain
+ */
---
+++ nmccammon_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,9 @@
+/*
+ * Modified Parameters:
+ * Changed font and size for title, subtitle, and buttons.
+ * Gave the title some color
+ * Changed the background
+ * "Game" scene has something instead of just being nothing
+ * Changed button hover and click colors
+ * Exit Game button turns red instead of exiting the game, you are not allowed to quit Minecraft 2
+ */
