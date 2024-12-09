6ab9ea9d659b631d84f81bcef282e2aa7cddf52e
noahmccammon <noahmccammon@gmail.com>
2024-12-08 20:18:47 -0800

    Hour 20

---
+++ nmccammon_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,22 @@
+/*
+     dBBBBb dBBBBBb     dBBBBBBb  dBBBP  dBBBP dBBBBBb    dBP dBBBBBBP dBP dBBBP
+                 BB          dBP                   dBP                          
+   dBBBB     dBP BB   dBPdBPdBP dBBP   dBP     dBBBBK   dBP    dBP   dBP dBP    
+  dB' BB    dBP  BB  dBPdBPdBP dBP    dBP     dBP  BB  dBP    dBP   dBP dBP     
+ dBBBBBB   dBBBBBBB dBPdBPdBP dBBBBP dBBBBP  dBP  dB' dBP    dBP   dBP dBBBBP   
+
+Is the game too easy or hard? Give 2 unique reasons why it is easy or hard.
+    The game is too easy in my opinion. The powerups are plentiful and the obstacles slowing you down doesn't make it that much harder to collect them.
+What 2 changes would you do to make it easier or harder?
+    To make the game harder I would have the obstacles do damage to the player, or subtract time. I'd also speed the game up a bit, or have the game get faster as it goes longer.
+List 2 things that would give it a "wow" factor.
+    I think adding a jump, and new pit obstacles would increase the "wow" factor. I think the ground and player should move faster as well, the game looks very slow at the moment.
+What 2 parts of the game are fun? 
+    The powerups are fun to collect, and it's nice to see how long you can go.
+What 2 parts of the game are tedious?
+     Because the game doesn't speed up and the obstacles don't really hinder you in any way, the game gets old pretty quick. I played for almost 6 minutes, and an entire minute of that was me actively avoiding powerups so the game would end.
+
+Changes Made:
+    Powerup: Changed to a gold color and altered the particles a bit to make it look shinier
+    Obstacle: Turned it into lasers coming down from the ceiling instead of just red cubes
+*/
